38dca84b4423b9490a530aad801687d0951edd20
Sekoda MasGaNo <masgano@gmail.com>
2019-02-19 08:42:01 +0400

    Fix variable

--- src/item.js
+++ src/item.js
@@ -12,7 +12,7 @@ Item.prototype.getDescription = function() {
   if (description.length > 30) {
     description = description.substr(0, 30);
   }
-  var output = "<p>" + this.description + "</p>"
+  var output = "<p>" + description + "</p>"
   return output
 }
 
